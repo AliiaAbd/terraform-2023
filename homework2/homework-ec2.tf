8d52dab1e1e4b921ad984fb86b3a2234523f6841
AliiaAbd <aliiaabdrakhmanova@gmail.com>
2023-12-21 21:43:49 +0000

    homework2

---
+++ homework2/homework-ec2.tf
@@ -0,0 +1,58 @@
+data "aws_ami" "amazon" {
+  most_recent      = true
+  owners           = ["137112412989"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023.*-x86_64"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+}
+
+
+
+# data "aws_ami" "amazon" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["099720109477"] # Canonical
+# }
+
+resource "aws_instance" "hw" {
+  ami           = data.aws_ami.amazon.id
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name= aws_key_pair.deployer.key_name
+  user_data = file ("apache.sh")
+  tags = {
+    Name = "Homework-ec2"
+  }
+}
+resource "aws_instance" "manual-instance" {
+  name = "manual-instance"
+ 
+}
+
+resource "aws_security_group" "launch-wizard-1" {
+  name = "launch-wizard-1"
+ 
+}
--- homework2/key.tf
+++ homework2/key.tf
@@ -1,8 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "homework-key"
   public_key = file("~/.ssh/id_rsa.pub")
-}
 
-output key {
-    value= aws_key_pair.deployer.key_type
+  
 }
